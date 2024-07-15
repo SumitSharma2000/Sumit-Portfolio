462111f94be69451df81ec6d0a67bf61644caa11
Sumit Sharma <sumitsharma7624@gmail.com>
2024-07-15 23:45:16 +0530

    add demo and github

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,38 +1,57 @@
 import React from "react";
-import RecipeApp from "../assets/portfolio/RecipeApp.png"
-import ChatApp from "../assets/portfolio/ChatApp.png"
-import ExpenseTracker from "../assets/portfolio/ExpenseTracker.png"
-import TodoApp from '../assets/portfolio/todo.png'
-import BlogWebsite from '../assets/portfolio/Blog Website.png'
-import CredClone from '../assets/portfolio/CredClone.png'
-
+import RecipeApp from "../assets/portfolio/RecipeApp.png";
+import ChatApp from "../assets/portfolio/ChatApp.png";
+import ExpenseTracker from "../assets/portfolio/ExpenseTracker.png";
+import TodoApp from '../assets/portfolio/todo.png';
+import BlogWebsite from '../assets/portfolio/Blog Website.png';
+import CredClone from '../assets/portfolio/CredClone.png';
+import WeatherWise from '../assets/portfolio/WeatherWise.png';
+import ZaikaBazaar from '../assets/portfolio/ZaikaBazaar.png';
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
       src: RecipeApp,
+      demo: "https://recipe-by-sumit.vercel.app",
+      github: "https://github.com/SumitSharma2000/Recipe-By-Sumit",
     },
     {
       id: 2,
       src: ChatApp,
+      demo: "https://chat-app-by-sumit.vercel.app/",
+      github: "https://github.com/SumitSharma2000/Chat-App",
     },
     {
       id: 3,
       src: ExpenseTracker,
+      demo: "https://expense-tracker-delta-umber.vercel.app/",
+      github: "https://github.com/SumitSharma2000/Expense-Tracker",
     },
     {
       id: 4,
-      src: TodoApp,
+      src: BlogWebsite,
+      demo: "https://modern-blog-website-react-redux-tail-wind.vercel.app/",
+      github: "https://github.com/SumitSharma2000/Modern-Blog-Website-React-Redux-TailWind",
     },
     {
       id: 5,
-      src: BlogWebsite,
+      src: CredClone,
+      demo: "https://cred-clone-mu-blond.vercel.app/",
+      github: "https://github.com/SumitSharma2000/Cred-Clone",
     },
     {
       id: 6,
-      src: CredClone,
+      src: WeatherWise,
+      demo: "https://weatherwisebysumit.netlify.app/",
+      github: "https://github.com/SumitSharma2000/WeatherWise",
     },
+    {
+      id: 7,
+      src: ZaikaBazaar,
+      demo: "https://zaika-bazaar.vercel.app",
+      github: "https://github.com/SumitSharma2000/ZaikaBazaar",
+    }
   ];
 
   return (
@@ -49,7 +68,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, demo, github }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -57,12 +76,22 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <a
+                  href={demo}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 text-center bg-blue-500 rounded-md text-white"
+                >
                   Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                </a>
+                <a
+                  href={github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 text-center bg-gray-800 rounded-md text-white"
+                >
                   Code
-                </button>
+                </a>
               </div>
             </div>
           ))}
